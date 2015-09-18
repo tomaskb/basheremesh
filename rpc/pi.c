e1a6c958375c3b571d9d79fa6b71c8b374304284
Francisco Jose Monaco <monaco@icmc.usp.br>
2015-09-18 13:44:27 -0300

    Done

--- rpc/pi.c
+++ rpc/pi.c
@@ -25,6 +25,7 @@
 int main (int argc, char **argv)
 {
 
-  
+  printf ("3.14\n");
+
   return EXIT_SUCCESS;
 }
--- rpc/stuff-rpc.c
+++ rpc/stuff-rpc.c
@@ -1,4 +1,4 @@
-/* stuff-rpc.c - Very Simple RPC Server, vr. 01
+/* stuff-rpc.c - Very Simple RPC client
 
    Copyright 2014  Monaco F. J.   <monaco@icmc.usp.br>
 
@@ -24,37 +24,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stuff.h>
+#include <rpc.h>
 #include <debug.h>
 
 /* See below. */
-#define BUFFSIZE 512
-#define CONFFIELDS 512
-#define CONFIGFILE "rpc.config"
-int init_connection (char *hosntame, int port);
-void end_connection (int fd);
-void read_config (void);
-char *getconf (const char* );
-char config[CONFFIELDS][BUFFSIZE][BUFFSIZE];
-int confflag=0;
+
+#define SERVER getconf ("server")
+#define PORT atoi(getconf ("port"))
 
 /* 
    Remote functions.
  */
 
+
 /* Return the value of PI. */
 
 float pi(void)
 {
+  char buffer[BUFFSIZE];
+
   read_config();
-  fatal (1, "Not implemented");
-  return 0;
+
+  call_remote (SERVER, PORT, "pi", buffer);
+
+  return atof(buffer);
 }
 
 /* Return n+1. */
 
 int plusone (int n)
 {
-  read_config();
   fatal (1, "Not implemented");
   return 0;
 }
@@ -63,68 +62,8 @@ int plusone (int n)
 
 void increment (int *n)
 {
-  read_config();
   fatal (1, "Not implemented");
 }
 
-/* 
-   RPC mechanism. 
-*/
 
-/* Connect to an RPC server at host 'hostname', port 'port' and
-   return a socket file descriptor. On error, return -1. */
-
-int init_connection (char *hosntame, int port)
-{
-  fatal (1, "Not implemented");
-  return 0;
-}
-
-/* End the connection associated to the open file descriptor  fd.*/
-
-void end_connection (int fd)
-{
-  fatal (1, "Not implemented");
-
-}
-
-/* Read configuration file (once). */
-
-void read_config (void)
-{
-  FILE *file;
-  char name[BUFFSIZE], value[BUFFSIZE];
-  int i;
-
-  if (confflag)
-    return;
-  else
-    confflag=1;
-
-  i=0;
-
-  file = fopen (CONFIGFILE,"r");
-  sysfatal (!file);
-  while ((i<CONFFIELDS) && (fscanf (file, "%s : %s", name, value) != EOF))
-    {
-      strncpy(config[i][0], name,  BUFFSIZE-1);
-      strncpy(config[i][1], value, BUFFSIZE-1);
-      i++;
-    }
-  
-}
-
-/* Get value of config field 'name'. */
-
-char *getconf (const char* name)
-{
-  int i;
-  i=0;
-  while ( (i<CONFFIELDS) && (strcmp(config[i][0],name)))
-    i++;
-  if (i<CONFFIELDS)
-    return config[i][1];
-  else
-    return NULL;
-}
 
