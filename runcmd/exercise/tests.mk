5e031650384a92b9476cf62e9828794cdf1fc502
Francisco Jose Monaco <monaco@icmc.usp.br>
2016-04-18 12:42:35 -0300

    General code improvements, several changes

--- runcmd/exercise/tests.mk
+++ runcmd/exercise/tests.mk
@@ -1,12 +1,23 @@
+# Run your tests
+#
 
-# Some tests
+# Some tests for runcmd
 
-test:	all
+tests-bin = test-runcmd
+
+tests_setup:
 	@rm -f nosuchfile *.log
-	@echo "\nRunning tests...\n"
+
+test:	all tests_setup
+	@echo "\nRunning tests...\n" 
 	@total=0;\
 	for i in $(tests-bin); do echo;\
 	./$$i; nerrors=$$?; total=$$(( $$total + $$nerrors)); done; \
 	echo; \
 	if test $$total -eq 0 ; then echo -n "Good! All tests ok!"; else echo -n "Found $$total error(s). Some tests failed."; fi;
 	@echo; echo
+
+.PONY : tests_setup
+
+
+
