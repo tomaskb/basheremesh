82442b3a6d1fa32999dadadd88c7990c6862f899
Moanco F. J <monaco@icmc.usp.br>
2016-04-24 15:06:24 -0300

    Adding Makefine

---
+++ utils/Makefine/config.mk
@@ -0,0 +1,76 @@
+# Project configuration
+
+# 
+# CUSTOMIZE FOR YOUR PROJECT
+#
+
+# Give your project a name, e.g.
+# PROJECT = FooBar
+
+# Name your project's binaires, eg.
+# bin = foo bar
+
+# For each item in $(bin), name the objects needed to build it, e.g.
+# foo_obj = main_foo.o 
+# bar_obj = main_bar.o 
+
+# For each item in $(bin), name the project's libraries to which it shall be linked, e.g.
+# foo_lib = libfoobar
+# bar_lib = libfoobar libm
+
+# For each item in $(bin), name the other libraries to which it shall be 
+# linked, e.g.
+# foo_lib_other = libfoobar
+# bar_lib_other = libfoobar libm
+
+
+# Name your project's libraries (to be installed), e.g.
+# lib = libfoobar
+
+# For each item in $(lib), name the objects needed to build it, e.g.
+# libfoobar_obj = foo.o bar.o
+
+# For each item in $(lib), name the headers your project installs, e.g.
+#libfoobar_h = foos.h
+
+# Name your project's auxuliary binaries (not to be installed), e.g.
+# noinst_bin = example
+
+# For each item in $(noinst_bin), name the objects needed to build it, e.g.
+# example_obj = example.o 
+
+# For each item in $(noinst_bin), name the libraries to which it's to be 
+# linked, e.g.
+# example_lib = libfoobar
+
+# Extra files to go in the distribution (add as needed), e.g.
+# EXTRA_DIST = foo.txt
+
+# List the automatically generated, distributed source files of any kind.
+# These files are generated in the development environment but should
+# be already available in the building enviroment such as configuration 
+# scripts etc, e.g.
+# gendist_files = config.h
+
+# Installation prefix, e.g.
+# PREFIX=./local
+
+# Customize the building environemnt (modify as desired), e.g.
+# CC = gcc
+# CPP_FLAGS = -I. -Wall --ansi --pedantic-errors -D_POSIX_C_SOURCE=200112L 
+# C_FLAGS = 
+# LD_FLAGS = -L.
+# MAKE = make
+# AR = ar
+
+# Name your test programns, if any e.g.
+# tests-bin = testfoo
+
+# Actions to be performed before the test suite, e.g.
+#tests_setup:
+#	@rm -f foo.log
+
+# Housekeeping (name temporary files to clean) e.g.
+# EXTRA_GARBAGE = *~
+# EXTRA_GARBAGE = *~ \#*
+
