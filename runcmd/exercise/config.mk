3fc1ff20cb882e6eba3cb06981496fdc0b7f57d8
Moanco F. J <monaco@icmc.usp.br>
2016-04-29 13:21:26 -0300

    Updated CPP_FLAGS with last POSIX revision

--- runcmd/exercise/config.mk
+++ runcmd/exercise/config.mk
@@ -71,7 +71,7 @@ PREFIX=./local
 # Customize the building environemnt (modify as desired)
 
 CC = gcc
-CPP_FLAGS = -Wall --ansi --pedantic-errors -D_POSIX_C_SOURCE=200112L 
+CPP_FLAGS = -Wall --ansi --pedantic-errors -D_POSIX_C_SOURCE=200809L 
 C_FLAGS = 
 LD_FLAGS = 
 MAKE = make
