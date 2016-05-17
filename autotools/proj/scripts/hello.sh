8f771db6c5cedf62ef7bb0ba297f4f03afe4ff06
Moanco F. J <monaco@icmc.usp.br>
2016-05-17 11:52:58 -0300

    Added autootols directory

---
+++ autotools/proj/scripts/hello.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#   hello.sh - A script.
+#
+#   Copyright (c) 2014, Francisco Jose Monaco  <moanco@icmc.usp.br>
+#
+#   This file is part of POSIXeg
+#
+#   POSIXeg is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+echo "This small script outputs this short message, and that's all."
+echo "Script hello.sh is part of POSIXeg (POSIX exempli gratia)"
+echo "Copyright (c) 2014, Francisco Jose Monaco (monaco@icmc.usp.br)"
+echo "POSIXeg is free software and may be distributed under the terms"
+echo "of the GNU General Public Licence vr. 3 or any later version."
