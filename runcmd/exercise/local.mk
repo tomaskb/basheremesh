be2363e572606a759e8a2d845678adc9e3d3d20d
Monaco F. J <monaco.fj@gmail.com>
2017-04-29 22:31:06 -0300

    Removed old runcmd

--- runcmd/exercise/local.mk
+++
@@ -1,9 +0,0 @@
-# Project-specific rules
-
-# Use this rule to test another lib implementatnion
-
-testfoo:
-	$(MAKE)
-	rm -f $(tests-bin:%=%.o)
-	$(MAKE) CPPFLAGS:="-I/tmp/foo/include" LDFLAGS:="-L/tmp/foo/lib -Wl,-rpath=/tmp/foo/lib" CFLAGS:="-g" test
-
